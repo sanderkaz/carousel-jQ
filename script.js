800c3965db76d161c2895f20761a0a89f5f238cb
Sandra Zaniewska <sandra.zaniewska@gmail.com>
2016-07-08 16:02:46 +0200

    drugie

---
+++ script.js
@@ -0,0 +1,88 @@
+/*var carousel = $('.carousel ul');
+
+var carouselElements = carousel.children();
+
+var c = carouselElements.first().clone();
+carousel.append(c)
+
+carouselElements = carousel.children();
+
+var current = 0,
+    offset = - $('.carousel').width(); // -400
+
+
+function slide(isLeft) {
+  
+  if (isLeft) {
+    current--;
+  } else {     
+    current++;
+  }
+  
+  console.log(current)
+  
+  if (current <= 0 && isLeft) {
+    
+    
+    // TODO 
+    carousel.animate( {
+    marginLeft: '0px'
+    }, 500, function() {
+      
+      carousel.css('marginLeft', -2000);
+      current = 5;
+      
+    });
+      
+    
+    
+  } else if (current >= carouselElements.length - 1 ) {
+    
+    carousel.animate( {
+      marginLeft: (current*offset)+'px'
+    }, 500, function() {
+      
+      current = 0
+      carousel.css('marginLeft', 0);
+      
+    });
+
+  } else {
+    
+    carousel.animate( {
+      marginLeft: (current*offset)+'px'
+    }, 500);
+    
+  }
+}
+
+
+setInterval(slide , 5000);
+
+
+$('#prev').on('click', function(){
+  
+  slide(true);
+  
+});
+
+$('#next').on('click', function(){
+  
+  slide();
+  
+});*/
+$(function(){
+  var carouselList = $(".carousel ul");
+  setInterval(function changeSlide() {
+    carouselList.animate({
+      marginLeft: -400
+      }, 400, moveFirstSlide);
+  }, 3000);
+  function moveFirstSlide (){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem),
+    carouselList.css({marginLeft:0});
+}
+});
+  
